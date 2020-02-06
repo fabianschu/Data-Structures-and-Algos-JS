a77bd85f4d4925d8a98b975d276d4729e881b3b2
fabianschu <48454910+fabianschu@users.noreply.github.com>
2020-02-06 10:29:20 +0100

    implemented improved version of insertionSort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,20 +1,34 @@
 const {swap} = require('./swap');
 
+// const insertionSort = arr => {
+
+// for (let i = 1; i < arr.length; i++) {
+
+//     for(let j = i - 1; j >= 0; j--) {
+//         if(arr[i]< arr[j] && (arr[i] >= arr[j-1] || j === 0)) {
+//             //insert before arr[j]
+//             let tmp = arr[i];
+//             arr.splice(i, 1)
+//             arr.splice(j, 0, tmp)
+//         }   
+//     }
+
+// }
+//     return arr;
+// }
+
 const insertionSort = arr => {
 
-for (let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) {
+        let tmp = arr[i];
 
-    for(let j = i - 1; j >= 0; j--) {
-        if(arr[i]< arr[j] && (arr[i] >= arr[j-1] || j === 0)) {
-            //insert before arr[j]
-            let tmp = arr[i];
-            arr.splice(i, 1)
-            arr.splice(j, 0, tmp)
-        }   
+        for(var j = i - 1; j >= 0 && arr[j] > tmp; j--) {
+            arr[j+1] = arr[j];
+        }
+        //j has been incremented in the loop before
+        arr[j+1] = tmp;
     }
-
-}
     return arr;
-}
+    }
 
 module.exports.insertionSort = insertionSort;
