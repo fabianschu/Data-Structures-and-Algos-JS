b7c7ebcdd8abf276aa880872c903016424aec8f8
fabianschu <48454910+fabianschu@users.noreply.github.com>
2020-02-12 17:47:34 +0100

    implemented pop() for doubly linked list

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -28,7 +28,18 @@ class DoublyLinkedList {
         return this;
     }
     pop(){
-
+        if (this.length === 0) return undefined;
+        let poppedNode = this.tail;
+        if (this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = poppedNode.prev;
+            this.tail.next = null;
+            poppedNode.prev = null;
+        }
+        this.length--;
+        return poppedNode;
     }
     shift(){
 
