dd83776c73f8f9a98f727e00b798b387950b3a5c
fabianschu <48454910+fabianschu@users.noreply.github.com>
2020-02-12 17:24:57 +0100

    implemented push() for doubly linked list

---
+++ dataStructures/doublyLinkedList.js
@@ -0,0 +1,55 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class DoublyLinkedList {
+    
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    
+    push(val){
+        let newNode = new Node(val);
+        if(this.length === 0) {
+            this.head = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+        }
+        this.tail = newNode;
+        this.length++;
+        return this;
+    }
+    pop(){
+
+    }
+    shift(){
+
+    }
+    unshift(){
+
+    }
+    get(){
+
+    }
+    set(){
+
+    }
+    insert(){
+        
+    }
+    remove(){
+
+    }
+}
+
+let list = new DoublyLinkedList();
+list.push('Teckel Head');
+console.log(list);
