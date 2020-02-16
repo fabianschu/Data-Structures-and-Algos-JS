80a743c54d2c9774aeb6f3857799f4575840224a
fabianschu <48454910+fabianschu@users.noreply.github.com>
2020-02-16 19:43:04 +0100

    implemented extractMax for binaryHeap

--- dataStructures/maxBinaryTree.js
+++ dataStructures/maxBinaryTree.js
@@ -8,14 +8,49 @@ class MaxBinaryHeap {
 
     insert(val) {
         this.values.push(val);
-        let currentIndex = this.values.length - 1;
+        let currentIdx = this.values.length - 1;
 
-        while(this.values[currentIndex] > this.values[Math.floor((currentIndex-1)/2)]) {
-            swap(this.values ,currentIndex, Math.floor((currentIndex-1)/2));
-            currentIndex = Math.floor((currentIndex-1)/2);
+        while(this.values[currentIdx] > this.values[Math.floor((currentIdx-1)/2)]) {
+            swap(this.values ,currentIdx, Math.floor((currentIdx-1)/2));
+            currentIdx = Math.floor((currentIdx-1)/2);
         }
 
         return this.values;
+    }
+
+    extractMax() {
+        //remove first element and exchange it with the last array element 
+        let max = this.values[0];
+        this.values[0] = this.values[this.values.length - 1];
+        this.values.pop();
+
+        if (this.values.length != 0) {
+            let currentIdx = 0;
+            let length = this.values.length;
+            while (true) {
+                let currentEl = this.values[currentIdx];
+                let leftChildIdx = 2 * currentIdx + 1;
+                let rightChildIdx = 2 * currentIdx + 2;
+                let leftChild, rightChild;
+                let swapIdx = -Infinity;
+                if(leftChildIdx < length) {
+                    leftChild = this.values[leftChildIdx];
+                    if(leftChild > currentEl) {
+                        swapIdx = leftChildIdx;
+                    }
+                }
+                if(rightChildIdx < length) {
+                    rightChild = this.values[rightChildIdx];
+                    if(rightChild > currentEl && rightChild > leftChild) {
+                        swapIdx = rightChildIdx;
+                    }
+                }
+                if(swapIdx === -Infinity) break;
+                swap(this.values, currentIdx, swapIdx);
+                currentIdx = swapIdx;
+            }
+        }
+        return max;
     }
 }
 
@@ -28,4 +63,8 @@ heap.insert(27)
 heap.insert(12)
 heap.insert(55)
 heap.insert(199);
+//console.log(heap);
+// heap.extractMax();
+// console.log(heap);
+heap.extractMax();
 console.log(heap);
